5d690eb23b6d7796c1024cfe366c76d425acbbe7
gvinaykumar9642 <vinaykumaryadav9642@gmail.com>
2021-11-06 12:46:31 +0530

    java Project Commit

---
+++ GITHUB_Project/src/test/java/com/createcontactwithbaseclass/jp_01_reverseString.java
@@ -0,0 +1,18 @@
+package com.createcontactwithbaseclass;
+
+public class jp_01_reverseString {
+
+	public static void main(String[] args) {
+
+		String s="java";
+		String rev="";
+		
+		for(int i=s.length()-1; i>=0; i--)
+		{
+			rev=rev+s.charAt(i);
+		}
+  System.out.println(rev);
+
+	}
+
+}
